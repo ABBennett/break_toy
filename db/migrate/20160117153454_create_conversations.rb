a036ad8c0c8aec02d252502e04429c74c2d81f48
AlexBennett <alexanderbondbennett@gmail.com>
2016-01-17 15:08:55 -0500

    changed chats table to conversations table to match the walkthrough

---
+++ db/migrate/20160117153454_create_conversations.rb
@@ -0,0 +1,14 @@
+class CreateConversations < ActiveRecord::Migration
+  def change
+    create_table :conversations do |t|
+      t.string :title
+      t.integer :sender_id, null: false
+      t.integer :recipient_id, null: false
+
+      t.timestamps null: false
+    end
+
+    add_index :conversations, :sender_id
+    add_index :conversations, :recipient_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20160117160557) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "chats", force: :cascade do |t|
+  create_table "conversations", force: :cascade do |t|
     t.string   "title"
     t.integer  "sender_id",    null: false
     t.integer  "recipient_id", null: false
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(version: 20160117160557) do
     t.datetime "updated_at",   null: false
   end
 
-  add_index "chats", ["recipient_id"], name: "index_chats_on_recipient_id", using: :btree
-  add_index "chats", ["sender_id"], name: "index_chats_on_sender_id", using: :btree
+  add_index "conversations", ["recipient_id"], name: "index_conversations_on_recipient_id", using: :btree
+  add_index "conversations", ["sender_id"], name: "index_conversations_on_sender_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username",                            null: false
