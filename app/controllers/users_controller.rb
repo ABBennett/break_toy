3385ff5f5b29270064dacf05e500cf8606a18d32
AlexBennett <alexanderbondbennett@gmail.com>
2016-01-17 12:34:02 -0500

    index loops through all users but current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!
+
   def index
-    @users = User.all
+    @users = User.where.not("id = ?",current_user.id).order("created_at DESC")
   end
 
   def show
