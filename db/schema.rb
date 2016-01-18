6c29754cb662bf1136d351a5e6e05a013b8e5e95
AlexBennett <alexanderbondbennett@gmail.com>
2016-01-18 13:38:20 -0500

    changed messages migration to match the one in chatty2 branch so migrations could roll back

--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,11 @@ ActiveRecord::Schema.define(version: 20160117204725) do
   enable_extension "plpgsql"
 
   create_table "conversations", force: :cascade do |t|
-    t.integer  "sender_id"
-    t.integer  "recipient_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string   "title"
+    t.integer  "sender_id",    null: false
+    t.integer  "recipient_id", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   add_index "conversations", ["recipient_id"], name: "index_conversations_on_recipient_id", using: :btree
@@ -30,8 +31,8 @@ ActiveRecord::Schema.define(version: 20160117204725) do
     t.text     "body"
     t.integer  "conversation_id"
     t.integer  "user_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
@@ -56,6 +57,4 @@ ActiveRecord::Schema.define(version: 20160117204725) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  add_foreign_key "messages", "conversations"
-  add_foreign_key "messages", "users"
 end
