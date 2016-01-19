263792e252ba80f524d974cd86828021bf7d1adc
AlexBennett <alexanderbondbennett@gmail.com>
2016-01-18 21:57:48 -0500

    fixed some top bar stuff and added users index test for not signed in user

--- spec/features/user_visits_converation_show_spec.rb
+++ spec/features/user_visits_converation_show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature "visitor visits a conversation show page" do
-  context "clicking on the Start Conversation button" do
+  context "signed in user clicking on the Start Conversation button" do
     let(:user) { FactoryGirl.create(:user) }
     let!(:user2) { FactoryGirl.create(:user) }
 
@@ -13,9 +13,23 @@ feature "visitor visits a conversation show page" do
       click_button 'Log in'
     end
 
-    scenario "user clicks 'Start Conversation'" do
+    scenario "signed in user clicks 'Start Conversation'" do
       click_button("Start Conversation")
       expect(page).to have_content("Chatroom")
+    end
+  end
+
+  context "not signed in user" do
+    let!(:user) { FactoryGirl.create(:user) }
+    let!(:user2) { FactoryGirl.create(:user) }
+
+    scenario "not signed in user clicks 'Start Conversation'" do
+      visit root_path
+      save_and_open_page
+      first(:button, "Start Conversation").click
+
+      expect(page).to have_content("You must sign in to start a conversation")
+      expect(page).to_not have_content("Chatroom")
     end
   end
 end
