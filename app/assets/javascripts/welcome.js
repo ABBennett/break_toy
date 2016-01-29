80aa051d561251e2d31cececae1fdbf5c587992d
AlexBennett <alexanderbondbennett@gmail.com>
2016-01-28 21:30:45 -0500

    fixed welcome added jquery to welcome and numbers on user show

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,11 @@
+$(document).ready(function (){
+  $("#welcome-signup").css("opacity", 0).animate({ opacity: 1}, 5000);
+  $("#welcome-video").css("opacity", 0).animate({ opacity: 1}, 1000);
+  // $("#welcome-description").css("opacity", 0).animate({ opacity: 1}, 2000);
+  // setTimeout(function() {
+  //     $("#app-info").css("opacity", 0).animate({ opacity: 1}, 2000);
+  // }, 0);
+
+
+
+});
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,22 +5,22 @@ class UsersController < ApplicationController
     @conversation = Conversation.new
     if params[:order] == "average"
       @users = User.all.sort{ |b,a| a.average_score <=> b.average_score }
-      @memo = ""
+      @memo = "Classiest: This ranks users by average talking points"
     elsif params[:order] == "averagedesc"
       @users = User.all.sort{ |a,b| a.average_score <=> b.average_score }
-        @memo = ""
+        @memo = "Trashiest: This ranks users by average talking points in reverse"
     elsif params[:order] == "messagecount"
         @users = User.all.sort{ |b,a| a.message_count <=> b.message_count}
-        @memo = ""
+        @memo = "Chattiest: This ranks users by the number of messages they've sent"
     elsif params[:order] == "messagecountdesc"
         @users = User.all.sort{ |a,b| a.message_count <=> b.message_count}
-        @memo = ""
+        @memo = "Quietest: This ranks users by the number of messages they've sent in reverse"
     elsif params[:order] == "ttlpts"
       @users = User.all.sort{ |b,a| a.total <=> b.total }
-        @memo = ""
+        @memo = "Pointiest: This ranks users by their total accumulated talking points"
     elsif params[:order] == "totaldesc"
       @users = User.all.sort{ |a,b| a.total <=> b.total }
-        @memo = ""
+        @memo = "Flatest: This ranks users in reverse order by their total accumulated talking points"
     else
       if user_signed_in?
         @users = User.where.not("id = ?",current_user.id).order("created_at DESC")
