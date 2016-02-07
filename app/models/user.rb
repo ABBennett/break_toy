d4b6160fd91621228e7312182aa4b4a7accde1c0
AlexBennett <alexanderbondbennett@gmail.com>
2016-02-07 13:37:25 -0500

    added in half colors to score_color user method

--- app/models/user.rb
+++ app/models/user.rb
@@ -88,24 +88,46 @@ class User < ActiveRecord::Base
   end
 
   def score_color
-    if self.average_score >= 9.0
+    if self.average_score == 10.0
+      "ten"
+    elsif self.average_score >= 9.5
+      "nine-5"
+    elsif self.average_score >= 9.0
       "nine"
+    elsif self.average_score >= 8.5
+      "eight-5"
     elsif self.average_score >= 8.0
       "eight"
+    elsif self.average_score >= 7.5
+      "seven-5"
     elsif self.average_score >= 7.0
       "seven"
+    elsif self.average_score >= 6.5
+      "six-5"
     elsif self.average_score >= 6.0
       "six"
+    elsif self.average_score >= 5.5
+      "five-5"
     elsif self.average_score >= 5.0
       "five"
+    elsif self.average_score >= 4.5
+      "four-5"
     elsif self.average_score >= 4.0
       "four"
+    elsif self.average_score >= 3.5
+      "three-5"
     elsif self.average_score >= 3.0
       "three"
+    elsif self.average_score >= 2.5
+      "two-5"
     elsif self.average_score >= 2.0
       "two"
+    elsif self.average_score >= 1.5
+      "one-5"
     elsif self.average_score >= 1.0
       "one"
+    elsif self.average_score >= 0.5
+      "zero-5"
     else
       "zero"
     end
